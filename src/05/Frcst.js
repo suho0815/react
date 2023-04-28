2e7a9ccc463893990d5cc5cbf26d4d35da7076f2
suho <suho0815@naver.com>
2023-04-28 17:30:38 +0900

    미세먼지

--- src/05/Frcst.js
+++ src/05/Frcst.js
@@ -1,14 +1,67 @@
 import data from './dataFrcst.json';
+import style from './Frcst.module.css';
+
+import { useState } from 'react';
 
 const Frcst = () => {
     const dtKey = ["frcstOneDt", "frcstTwoDt", "frcstThreeDt", "frcstFourDt"];
+    const cnKey = ["frcstOneCn", "frcstTwoCn", "frcstThreeCn", "frcstFourCn"];
 
-    dtKey.map((item) => console.log(data[item]));
+    // dtKey.map((item) => console.log(data[item]));
+    // cnKey.map((item) => console.log(data[item]));
     
-    console.log(data);
-    return (
-        <main>
+    let dtcn = {};
+    dtKey.map((item, idx) => 
+        dtcn[data[item]] = data[cnKey[idx]]
+        );
 
+    const [cn, setcn] = useState('');
+    const [sel, setsel] = useState('');
+
+    // 상세정보
+    const detail = (k) =>{
+        
+        setsel(k);
+
+        let dtcnItem = dtcn[k].split(',');
+        dtcnItem = dtcnItem.map((item)=> item.split(':'));
+        dtcnItem = dtcnItem.map((item) => 
+                    <div key={item[0]}>
+                        <span className={style.sp1}>{item[0]}</span>
+                        {item[1].trim() === '낮음' ? <span className={style.sp21}>{item[1]}</span>
+                        : item[1].trim() === '보통' ? <span className={style.sp22}>{item[1]}</span>
+                        : <span className={style.sp23}>{item[1]}</span> }
+                    </div>
+        );        
+        setcn(dtcnItem);
+
+    };
+
+
+    // 요일
+    let dtTag = [];
+    dtTag = Object.keys(dtcn).map((item, idx) => 
+            <div className={sel === item ? style.dt : style.dt1} 
+            key={'dt' + idx}
+            onClick={()=> detail(item)}>{item}</div>
+    );
+    
+
+    // console.log(data);
+    return (
+        <main className='container'>
+            <article>
+                <header>
+                    <h2>초미세먼지 주간예보</h2>
+                    <div className="grid">
+                        {dtTag}
+                    </div>
+                </header>
+                <div className="grid">
+                    {cn}
+                </div>
+            </article>
+            
         </main>
     );
 }
