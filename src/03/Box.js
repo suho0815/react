ca55a1054fbe9befba92c7b6b3798b20074ec4ab
suho <suho0815@naver.com>
2023-05-16 14:30:31 +0900

    firebase hosting

--- src/03/Box.js
+++ src/03/Box.js
@@ -40,7 +40,7 @@ const Box = () => {
 
         //데이터 가져오기
         const getData = (sd) => {
-            let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=" + sd;
+            let url = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=" + sd;
             console.log("url = ", url);
 
             fetch(url)
--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 // import Hello from './01/Hello' ;
 // import MyClock from './02/MyClock';
-// import Box from './03/Box';
+import Box from './03/Box';
 // import MyDiv from './04/MyDiv';
 // import Frcst from './05/Frcst';
 // import Taccident from './06/Taccident'
 // import MyRef from './07/MyRef';
-import Gallery from './08/Gallery';
+// import Gallery from './08/Gallery';
 function App() {
   return (
     // <Hello />
     // <MyClock/>
-    // <Box/>
+    <Box/>
     // <MyDiv/>
     // <Frcst/>
     // <Taccident/>
     // <MyRef/>
-    <Gallery/>
+    // <Gallery/>
   );
 }
 
